7d9abac197551b7006a0cdc7df4e2edbcee4fe20
“用dengjiankai <dengjiankai@wanda.cn>
2017-10-15 20:52:30 +0800

    commit

--- dzframework/src/main/java/com/dengzi/dzframework/skin/attr/SkinAttr.java
+++ dzframework/src/main/java/com/dengzi/dzframework/skin/attr/SkinAttr.java
@@ -4,13 +4,14 @@ import android.view.View;
 
 /**
  * @author Djk
- * @Title:
+ * @Title: 属性bean
  * @Time: 2017/10/13.
  * @Version:1.0.0
  */
 public class SkinAttr {
-
+    // 皮肤资源名称
     private String mResourceName;
+    // 皮肤资源属性Type
     private SkinType mSkinType;
 
     public SkinAttr(String resourceName, SkinType skinType) {
--- dzframework/src/main/java/com/dengzi/dzframework/skin/attr/SkinAttrSupport.java
+++ dzframework/src/main/java/com/dengzi/dzframework/skin/attr/SkinAttrSupport.java
@@ -16,23 +16,28 @@ import java.util.List;
  */
 public class SkinAttrSupport {
 
+    /**
+     * 获取皮肤view对应的属性（一个textview可能会对应多个要换肤的属性，比如background和textcolor）
+     *
+     * @param context
+     * @param attrs
+     * @return
+     */
     public static List<SkinAttr> getSkinAttr(Context context, AttributeSet attrs) {
         List<SkinAttr> skinAttrList = new ArrayList<>();
         int attrCount = attrs.getAttributeCount();
         for (int index = 0; index < attrCount; index++) {
             String attrName = attrs.getAttributeName(index);
             String attrValue = attrs.getAttributeValue(index);
-
-//            Log.e("dengzi", "attrName：" + attrName + "， attrValue：" + attrValue);
+            // Log.e("dengzi", "attrName：" + attrName + "， attrValue：" + attrValue);
             // attrName：textColor， attrValue：@2131558476
-
-            // 只获取我们关心的属性
+            // 只获取我们关心的属性 比如background和textcolor
             SkinType skinType = getSkinTypeByName(attrName);
             if (skinType != null) {
-                Log.e("dengzi", "skinType：" + skinType.name());
+                // 获取我们关心的属性所对应的资源名称名称
                 String resName = getResNameByValue(context, attrValue);
-                Log.e("dengzi", "resName：" + resName);
                 if (!TextUtils.isEmpty(resName)) {
+                    // 创建一个资源属性
                     SkinAttr skinAttr = new SkinAttr(resName, skinType);
                     skinAttrList.add(skinAttr);
                 }
@@ -50,8 +55,10 @@ public class SkinAttrSupport {
      */
     private static String getResNameByValue(Context context, String attrValue) {
         if (attrValue.startsWith("@")) {
+            // 去除前面的@
             attrValue = attrValue.substring(1);
             int resId = Integer.parseInt(attrValue);
+            // 获取当前id对应的
             return context.getResources().getResourceEntryName(resId);
         }
         return null;
